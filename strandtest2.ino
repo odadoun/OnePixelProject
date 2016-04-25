db17ab24e1e3999ee4dd90fd76b7bcd05c25d7e2
odadoun <odadoun@gmail.com>
2016-04-25 23:11:34 +0200

    First implementation of read file

--- strandtest2.ino
+++ strandtest2.ino
@@ -1,42 +1,23 @@
-// Simple strand test for Adafruit Dot Star RGB LED strip.
-// This is a basic diagnostic tool, NOT a graphics demo...helps confirm
-// correct wiring and tests each pixel's ability to display red, green
-// and blue and to forward data down the line.  By limiting the number
-// and color of LEDs, it's reasonably safe to power a couple meters off
-// the Arduino's 5V pin.  DON'T try that with other code!
+/* April 2016 odadoun@gmail.com */
+/*  Dadoun for One Pixel project  */
+/* Ibis project  in the following */
 #include <Adafruit_DotStar.h>
-// Because conditional #includes don't work w/Arduino sketches...
-#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
-//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+#include <SPI.h>       
 #include <SD.h>
 #define NUMPIXELS 64 // Number of LEDs in strip
-// Here's how to control the LEDs from any two pins:
 #define DATAPIN    5
 #define CLOCKPIN   6
-Adafruit_DotStar strip = Adafruit_DotStar(
-                           NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-// The last parameter is optional -- this is the color data order of the
-// DotStar strip, which has changed over time in different production runs.
-// Your code just uses R,G,B colors, the library then reassigns as needed.
-// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
-
-// Hardware SPI is a little faster, but must be wired to specific pins
-// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
-//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
+Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 const int  buttonPinB = 8;
-const int  buttonPinA = 9;// the pin that the encoder is attached to
-//const int ledPin = 13;       // the pin that the LED is attached to
+const int  buttonPinA = 9;
 const int ethPin = 10;
 const int sdPin = 4;
-// Variables will change:
-int buttonPushCounter = 0;   // counter for the number of encoder switch
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
-
+int buttonPushCounter = 0;   
+int buttonState = 0;         
+int lastButtonState = 0;    
 int increment_k2000 = 0;
 
 void setup() {
-
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
   clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
 #endif
@@ -54,11 +35,9 @@ void setup() {
   digitalWrite(sdPin, HIGH);
   //initialize serial communication:
   Serial.begin (9600);
+ 
 }
 
-// Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
-int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
 uint32_t color = 0xFF0000;      // 'On' color (starts red)
 //unsigned int *led_nb = (unsigned int*)malloc(NUMPIXELS * sizeof(unsigned int));
 //bool *seq = (bool*)malloc(NUMPIXELS * sizeof(bool));
@@ -79,8 +58,8 @@ void loop() {
       //increment();
       Serial.print("number of button pushes:  ");
       Serial.println(buttonPushCounter);
-      pos=K2000(buttonPushCounter);
-   
+      //pos=K2000(buttonPushCounter);
+       ReadIt();
       //all_pixels_off();
       //onestep(buttonPushCounter);
       /* From 1 to 63 */  
@@ -212,3 +191,39 @@ bool* fill_led_sequence()
   }
   return on_off;
 } 
+/**********************************/
+/* READ FILE                     */
+void ReadIt()
+{
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  File myFile = SD.open("test.txt");
+  String line;
+  int n=0;
+  if (myFile) {
+    Serial.println("test.txt:");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      line = myFile.readStringUntil('\n');
+      Serial.println(line);
+      for(unsigned int i=0;i<line.length()-1;i++)
+      {
+        strip.setPixelColor(i,line[i]);
+        Serial.println(i);
+        Serial.println(line[i]);
+      }
+      
+      delay(50);
+      n++;
+    }
+    // close the file:
+    myFile.close();
+ }
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
