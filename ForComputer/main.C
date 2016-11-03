bc8828e2f38ebb0d482ed1f2d0d9c87c57705740
DADOUN Olivier <olivier.dadoun@lpnhe.in2p3.fr>
2016-11-03 16:37:25 +0100

    Name and position of the galaxy added in the window

--- ForComputer/main.C
+++ ForComputer/main.C
@@ -9,44 +9,109 @@
 #include <csignal>
 #include <stdio.h>
 #include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <unistd.h>
 
 TheReaderUniverse reader_universe;
 char xy_RGB[5][64];
-
-//ofstream last_line_read("llr");
 fstream last_line_read;
-//std::ofstream last_line_read("llr");
 /* For OpenGL Utility Toolkit (GLUT) */
 float colorR = 0.0f;
 float colorG = 0.0f;
 float colorB = 0.0f;
+int WindowHeight = 300;
+int WindowWidth = 300;
 bool position_defined = false;
+void printtext(int x, int y, string String)
+{
+	//(x,y) is from the bottom left of the window
+	glColor3f(0,0,0);
+	glDisable(GL_LIGHTING);
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0, WindowWidth, 0, WindowHeight, -1.0f, 1.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+	glPushAttrib(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
+	glRasterPos2i(x,y);
+	for (int i=0; i<String.size(); i++)
+	{
+		//glutBitmapCharacter(GLUT_BITMAP_9_BY_15, String[i]);
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, String[i]);
+	}
+	glPopAttrib();
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnable(GL_DEPTH_TEST);
+
+	char string[64];
+	sprintf(string, "something");
+	printtext(10,10,string);
+
+	glutSwapBuffers();
+}
+
+/* ************************* */
+void signalHandler(int signum);
+void GetRGBUniverse();
+void renderScene();
+void timer(int value);
+/* ************************* */
+int main(int argc, char **argv)
+{
+	reader_universe.myFile.open("test1.txt", std::ifstream::in);
+	reader_universe.load_constellations_abacus();
+	signal(SIGINT, signalHandler);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WindowWidth,WindowHeight);
+	glutCreateWindow("One Pixel Universe");
+	glutDisplayFunc(renderScene);
+	glutTimerFunc( 0, timer, 0 );
+	glutMainLoop();
+	return 0;
+}
+/* ************************* */
 /* To catch CTRL C signal*/
-void signalHandler( int signum ) {
+void signalHandler(int signum) 
+{
 	cout << "Interrupt signal (" << signum << ") received.\n";
 	cout << "Write last line number read into llr file ..." << reader_universe.GetLinesRead() << endl;
 	// Catch last line read
 	last_line_read.open("llr",ios::out);
 	last_line_read << reader_universe.GetLinesRead();
 	last_line_read.close();
-        // cleanup and close up stuff here  
+	// cleanup and close up stuff here  
 	// terminate program  
 	exit(signum);  
 }
+/* ************************* */
 void GetRGBUniverse()
 {
-	if(position_defined == false) {
+	if(position_defined == false) 
+	{
 		unsigned long int line_position;
 		last_line_read.open("llr",ios::in);
 		if (last_line_read.is_open())
 		{
-		last_line_read >> line_position;
-		last_line_read.close();
+			last_line_read >> line_position;
+			last_line_read.close();
 		}
-		else line_position=0;
+		else 
+			line_position=0;
 		cout << " From the last line read, start @ line " << line_position <<endl;
-	        unsigned long int bytes_read=reader_universe.injection(line_position);	
+		unsigned long int bytes_read=reader_universe.injection(line_position);	
 		reader_universe.SetLinesRead(line_position);
 		reader_universe.SetBytesRead(bytes_read);
 		position_defined=true;
@@ -55,7 +120,7 @@ void GetRGBUniverse()
 
 	for(int i=0;i<=4;i++)
 	{
-		cout << xy_RGB[i] << endl;
+		cout << " xy RGB values : " << xy_RGB[i] << endl;
 	}
 
 	int n=reader_universe.GetLinesRead();
@@ -69,13 +134,14 @@ void GetRGBUniverse()
 	cout << "Constellation name  : " << name_const << endl;
 	cout << "Galactic coordinates "   <<  reader_universe.GetLongitude(px) << " " << reader_universe.GetLatitude(py) << endl;
 }
-void timer( int value )
+/* ************************* */
+void timer(int value)
 {
 	//changeColor?
 	GetRGBUniverse();
-	colorR = atof(xy_RGB[0])/255.;
-	colorG = atof(xy_RGB[1])/255.;
-	colorB = atof(xy_RGB[2])/255.;
+	colorR = atof(xy_RGB[2])/255.;
+	colorG = atof(xy_RGB[3])/255.;
+	colorB = atof(xy_RGB[4])/255.;
 	if (colorR > 1.0)
 		colorR = 0;
 	if (colorG > 1.0)
@@ -84,30 +150,26 @@ void timer( int value )
 		colorB = 0;
 
 	glutPostRedisplay();
-	glutTimerFunc( 10, timer, 0 );
+	int time_random;
+	time_random = 10 + rand()%1000;
+	cout << " Time random " << time_random << endl;
+	glutTimerFunc( time_random, timer, 0 );
 }
-
+/* ************************* */   
+/* ************************* */   
 void renderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glColor3f(colorR, colorG, colorB);
 	glRectf(-1.f,1.f, 1.f, -1.f);
+	unsigned long int px=strtoul(xy_RGB[0],NULL,0);
+	unsigned long int py=strtoul(xy_RGB[1],NULL,0);
+	string name_const = reader_universe.return_constellation(px,py);
+	cout << "Galactic coordinates "   <<  reader_universe.GetLongitude(px) << " " << reader_universe.GetLatitude(py) << endl;
+        char temp[256];
+	sprintf(temp,"        %f   %f",reader_universe.GetLongitude(px),reader_universe.GetLongitude(py));
+	string sentence = name_const + "       "  + string(temp);
+	printtext(5,10,sentence);
 	glutSwapBuffers();
 }
-
 /* ************************* */
-int main(int argc, char **argv)
-{
-	reader_universe.myFile.open("test1.txt", std::ifstream::in);
-	reader_universe.load_constellations_abacus();
-	signal(SIGINT, signalHandler);
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(320, 320);
-	glutCreateWindow("One Pixel Universe");
-	glutDisplayFunc(renderScene);
-	glutTimerFunc( 0, timer, 0 );
-	glutMainLoop();
-	glutMainLoop();
-	return 0;
-}
